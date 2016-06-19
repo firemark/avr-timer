36d5e1182d6a1078dabb6dd387805a7005d50c5a
Firemark <marpiechula@gmail.com>
2016-06-20 01:52:15 +0200

    setup interrupts

--- timer.c
+++ timer.c
@@ -16,8 +16,16 @@ const int NUMBERS[] = {
 };
 
 int setup(void) {
+    // setup ports
     DDRC = 0b00111111;
     DDRD = 0b10000011;
+    // setup timer
+    TCCR0 = 0b101; // set to clk/1024
+    TCNT0 = 0; // reset counter
+    TIMSK = 1; // set overflow interrupt
+    // setup external interrupts
+    MCUCR = 0b00001111; // rising edge interrupt
+    GICR = 0b11000000; // set int0, int1 interrupt
 }
 
 char get_pin_select(char seg) {
@@ -37,36 +45,48 @@ void clear_digit(char seg) {
 }
 
 char is_run = 0;
+char is_setup = 1;
 char timer = 0;
 
 ISR(INT0_vect) { // start/reset
-    if (is_run) {
+    if (is_setup) {
+        is_run = 1;
+    } else if (is_run) {
         is_run = 0;
         timer = 0;
+    }
+}
+
+ISR(INT1_vect) { // add/stop
+    if (is_setup) {
+        timer += 5;
+    } else if (is_run) {
+        is_run = 0;
     } else {
         is_run = 1;
     }
 }
 
-ISR(INT1_vect) { // add/stop
-    if (is_run) {
-        timer += 5;
-    } else {
-        is_run = 0;
-    }
-}
+int cycles = 0;
 
 ISR(TIM0_OVF_vect) {
-    //todo
+    if (cycles++ == 61) {
+        cycles = 0;
+    } else {
+        timer += 1;
+    }
 }
 
 int main(void) {
     setup();
-    for(;;){
+    for (;;) {
         set_digit(timer / 10, 0);
-        _delay_ms(5);
+        _delay_ms(2);
+        clear_digit(2);
+
         set_digit(timer, 1);
-        _delay_ms(5);
+        _delay_ms(2);
+        clear_digit(2);
     }
     return 0;
 }
